7a8181fa832e9dd7c05e2fa70f9d7eefd6b8055a
Denis Shcherba <Denis.Shcherba@galaktika.ru>
2025-09-03 12:53:25 +0400

    fix tests

--- Template/Tests/Template.Tests/UnitTest1.cs
+++ Template/Tests/Template.Tests/UnitTest1.cs
@@ -5,7 +5,13 @@ namespace Template.Tests
         [Fact]
         public void Test1()
         {
+            Assert.Equal(8, 8);
+        }
 
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(8, 5);
         }
     }
 }
